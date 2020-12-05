049539847d19a11e88fbfc3d03b2e8cbec2a3cb5
Justin Restivo <justin.p.restivo@gmail.com>
2020-12-04 21:55:01 -0500

    days 2 3 4 in the BOOKS

--- day1.hs
+++ day1.hs
@@ -2,19 +2,25 @@ import Data.Maybe
 import Control.Monad
 
   
-is_valid :: [Int] -> Bool
-is_valid a = sum a == 2020
+isValid :: [Int] -> Bool
+isValid a = sum a == 2020
 
-find_satisfying_sum :: Int -> [Int] -> [Int]
-find_satisfying_sum n l =
-  [product x | x <- replicateM n l, is_valid x]
   
-part_x n = 
+findSatisfyingSum :: Int -> [Int] -> [Int]
+findSatisfyingSum n l =
+  [product x | x <- replicateM n l, isValid x]
+  
+partX :: Int -> (String -> String)
+partX n s = 
                    let l = map read $ lines s in
-                   let valids = find_satisfying_sum n l in
-                   foldr (\x acc -> acc ++ "\n" ++ (show x)) "" valids
+                   let valids = findSatisfyingSum n l in
+                   foldr (\x acc -> acc ++ "\n" ++ show x) "" valids
 
-part_1 s = part_x 2
-part_2 s = part_x 3
+part_1 :: String -> String
+part_1 = partX 2
+  
+part_2 :: String -> String
+part_2 = partX 3
 
+main :: IO ()
 main = interact part_2
---
+++ day2.hs
@@ -0,0 +1,28 @@
+import Data.Set (fromList, member)
+import Data.List (groupBy)
+
+data Password = Password {numTimesMin :: Int, numTimesMax :: Int, character :: Char, password :: String} deriving Show
+
+isValidPart2 :: Password -> Bool
+isValidPart2 p =
+  foldr (/=) False (fmap (\x -> password p !! x == character p) [numTimesMin p - 1, numTimesMax p - 1])
+
+isValidPart1 :: Password -> Bool
+isValidPart1 p =
+  let len = length $ Prelude.filter (\x -> x == character p) (password p)  in
+  len >= numTimesMin p && len <= numTimesMax p
+
+parseString :: String -> Password
+parseString s =
+  let delimiters = fromList ['-', ' ', ':'] in
+    -- not my best work...
+  let parsed =  filter (\x -> not $ member (head x) delimiters) $ groupBy (\x c -> not (member x delimiters) && not (member c delimiters) ) s in
+  Password (read $ head parsed) (read $ parsed !! 1) (head $ parsed !! 2) (parsed !! 3)
+
+part_1 :: String -> String
+part_1 s = 
+                   let l = lines s in
+                   show $ length $ [x | x <- l, isValidPart2 $ parseString x]
+
+main :: IO ()
+main = interact part_1
