de8e6f4389ba17ae9eff6e1f05c70159814d2255
Justin Restivo <justin.p.restivo@gmail.com>
2020-12-22 16:43:16 -0500

    changed some things day19...

--- day19.hs
+++ day19.hs
@@ -1,35 +1,7 @@
-import           Control.Monad                  ( replicateM
-                                                , foldM
-                                                )
-import           Data.Maybe                     ( fromJust
-                                                , Maybe
-                                                )
-
-import           Data.Foldable                  ( foldl'
-                                                , fold
-                                                )
-import           Data.Set                       ( Set
-                                                , fromList
-                                                , singleton
-                                                , empty
-                                                , union
-                                                , member
-                                                , notMember
-                                                , cartesianProduct
-                                                -- , filter
-                                                , map
-                                                , toList
-                                                )
-import           Data.List                      ( uncons
-                                                , find
-                                                , elemIndex
-                                                )
-import           Data.Map                       ( insert
-                                                , singleton
-                                                , empty
-                                                , Map
-                                                , (!)
-                                                )
+import           Data.Foldable                  ( foldl' )
+import qualified Data.Set                      as S
+import qualified Data.List                     as L
+import qualified Data.Map                      as M
 
 data Rule = NT [Int] (Maybe [Int]) | T Char deriving Show
 
@@ -39,26 +11,25 @@ main = interact part_1
 part_1 :: String -> String
 part_1 s =
   let [rule_str, values] = mergeLines (/= []) unlines (lines s)
-      ruleMap            = foldl' (\acc x -> uncurry insert x acc)
-                                  Data.Map.empty
+      ruleMap            = foldl' (flip $ uncurry M.insert)
+                                  M.empty
                                   (parseRule <$> lines rule_str)
       useful_vals = getValues ruleMap 0
-      results     = filter (`member` useful_vals) $ lines values
+      results     = filter (`S.member` useful_vals) $ lines values
   in  show $ length results
 
-aggregateRule :: Map Int Rule -> [Int] -> Set String
+aggregateRule :: M.Map Int Rule -> [Int] -> S.Set String
 aggregateRule rmap rule_keys =
   let values = getValues rmap <$> rule_keys
-  in  foldl'
-        (\acc ss -> Data.Set.map (uncurry (++)) (cartesianProduct acc ss))
-        (Data.Set.singleton "")
-        values
+  in  foldl' (\acc ss -> S.map (uncurry (++)) (S.cartesianProduct acc ss))
+             (S.singleton "")
+             values
 
-getValues :: Map Int Rule -> Int -> Set String
-getValues rmap k = case rmap ! k of
-  NT r1 (Just r2) -> aggregateRule rmap r1 `union` aggregateRule rmap r2
+getValues :: M.Map Int Rule -> Int -> S.Set String
+getValues rmap k = case rmap M.! k of
+  NT r1 (Just r2) -> aggregateRule rmap r1 `S.union` aggregateRule rmap r2
   NT r1 Nothing   -> aggregateRule rmap r1
-  T c             -> Data.Set.singleton [c]
+  T c             -> S.singleton [c]
 
 mergeLines :: ([a] -> Bool) -> ([[a]] -> [a]) -> [[a]] -> [[a]]
 mergeLines f combine l = case span f l of
@@ -76,8 +47,9 @@ readRule r = read <$> splitBy (== ' ') r
 parseRule :: String -> (Int, Rule)
 parseRule s =
   let [ruleNum, rest] = splitBy (== ':') s
-  in  case elemIndex '\"' rest of
+  in  case L.elemIndex '\"' rest of
         Nothing -> case splitBy (== '|') rest of
           [r1]     -> (read ruleNum, NT (readRule r1) Nothing)
           [r1, r2] -> (read ruleNum, NT (readRule r1) (Just $ readRule r2))
+          _        -> error "incorrect formatting"
         Just indx -> (read ruleNum, T $ rest !! (indx + 1))
