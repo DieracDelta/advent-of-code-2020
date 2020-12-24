3ddaa59830640a99dab4fee06f3ad051c182454f
Justin Restivo <justin.p.restivo@gmail.com>
2020-12-23 19:13:13 -0500

    before vector implementation...

--- day23.hs
+++ day23.hs
@@ -1,64 +1,94 @@
-import           Data.Foldable                  ( foldl' )
+import           Data.Foldable                 as F
 import qualified Data.Set                      as S
 import qualified Data.List                     as L
 import qualified Data.Map                      as M
 import qualified Control.Monad                 as CM
 import qualified Data.Char                     as C
+import qualified Data.Maybe                    as DM
+import qualified Data.Vector                   as V
 
--- invariant: head of list is ALWAYS current cup
--- cups shift_distance [cups]
-data Cups = Cups Int Int [Int] deriving Show
-
-rotateBack :: Cups -> [Int]
-rotateBack (Cups sd _ cups) =
-  let len         = length cups
-      num_to_drop = len - (sd `rem` len)
-  in  take len $ drop num_to_drop (cycle cups)
+data Cups = Cups Int Int (M.Map Int Int) [Int] deriving Show
 
 main :: IO ()
 main = interact part_2
 
+showCups :: Cups -> String
+showCups (Cups cur_ele _ cmap s) =
+  (snd $ F.foldl'
+      (\acc _ ->
+        let element = DM.fromJust $ M.lookup (fst acc) cmap
+        in  (element, snd acc ++ "," ++ show element)
+      )
+      (cur_ele, "")
+      [1 .. 9]
+    )
+    ++ (show s)
+
 part_1 :: String -> String
 part_1 s =
   let parsedLine = parseInput s
-      Cups _ _ cups =
-          iterate performRound (Cups 0 (maximum parsedLine) parsedLine) !! 100
-      (tl, hd) = span (/= 1) cups
-  in  show $ tail hd ++ tl
+  in  show $ showCups $ iterate performRound parsedLine !! 100
 
 part_2 :: String -> String
 part_2 s =
-  let parsedLine = parseInput s ++ [10 .. 1000000]
-      Cups _ _ cups =
-          iterate performRound (Cups 0 (maximum parsedLine) parsedLine) !! 100
-          -- iterate performRound (Cups 0 (maximum parsedLine) parsedLine)
-          --   !! 10000000
-      (tl, hd) = span (/= 1) cups
-  in  show $ take 2 $ (tail hd ++ tl)
+  let (Cups h _ omap _) = parseInput s
+      start_cups        = Cups h 1000000 (append_to_input omap) []
+      (Cups _ _ mmap _) = iterate performRound start_cups !! 10000000
+  in  show $ mmap M.! 1
+
+append_to_input :: M.Map Int Int -> M.Map Int Int
+append_to_input omap =
+  let nmap =
+          F.foldl' (\acc ele -> M.insert ele (ele + 1) acc) omap [10 .. 999999]
+      nnmap = M.insert 7 10 nmap
+      f_map = M.insert 1000000 3 nnmap
+  in  f_map
 
 correctEle :: Int -> Int -> Int
 correctEle cur_attempt max_ele =
   if cur_attempt == 0 then max_ele else cur_attempt
 
 getDestCup :: Int -> Int -> S.Set Int -> Int
-getDestCup cur_attempt max_ele cups = if S.member cur_attempt cups
-  then getDestCup (correctEle (cur_attempt - 1) max_ele) max_ele cups
-  else cur_attempt
+getDestCup cur_attempt max_ele cups =
+  let corrected_ele = correctEle (cur_attempt - 1) max_ele
+  in  if S.member corrected_ele cups
+        then getDestCup corrected_ele max_ele cups
+        else corrected_ele
 
+findElement :: Int -> Int -> M.Map Int Int -> [Int]
+findElement start_ele num_eles mmap = if num_eles == 0
+  then []
+  else
+    let new_start_ele = DM.fromJust $ M.lookup start_ele mmap
+    in  new_start_ele : findElement new_start_ele (num_eles - 1) mmap
 
 performRound :: Cups -> Cups
-performRound (Cups sd max_ele cups) =
-  let (cur_cup, remaining) = splitAt 1 cups
-      (three_cups, rest) = splitAt 3 remaining
-      destination_cup = getDestCup (correctEle (head cur_cup - 1) max_ele)
-                                   max_ele
-                                   (S.fromList three_cups)
-      (s, e)      = span (/= destination_cup) (cur_cup ++ rest)
-      end_portion = case L.uncons e of
-        Nothing     -> three_cups
-        Just (a, b) -> a : (three_cups ++ b)
-      total_unrotated = s ++ end_portion
-  in  Cups (sd + 1) max_ele (tail total_unrotated ++ [head total_unrotated])
+performRound bleh@(Cups cur_ele max_ele cups _) =
+  let three_cups    = findElement cur_ele 3 cups
+      dest_cup      = getDestCup cur_ele max_ele (S.fromList three_cups)
+      dest_next_cup = DM.fromJust $ M.lookup dest_cup cups
+      first_cup     = head three_cups
+      last_cup      = last three_cups
+      end_cup       = DM.fromJust $ M.lookup last_cup cups
+      updated_map_1 = M.insert dest_cup first_cup cups
+      updated_map_2 = M.insert last_cup dest_next_cup updated_map_1
+      final_map     = M.insert cur_ele end_cup updated_map_2
+  in  Cups (DM.fromJust $ M.lookup cur_ele final_map)
+           max_ele
+           final_map
+           three_cups
 
-parseInput :: String -> [Int]
-parseInput s = read . CM.return <$> filter C.isDigit s
+
+parseInput :: String -> Cups
+parseInput s =
+  let inputs_list = read . CM.return <$> filter C.isDigit s :: [Int]
+      len         = L.length inputs_list
+      mmap =
+          F.foldl'
+            (\acc indx ->
+              let remainder = (indx + 1) `rem` len
+              in  M.insert (inputs_list !! indx) (inputs_list !! remainder) acc
+            )
+            M.empty
+            [0 .. len - 1] :: M.Map Int Int
+  in  Cups (head inputs_list) (maximum inputs_list) mmap []
